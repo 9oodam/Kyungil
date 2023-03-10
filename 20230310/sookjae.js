6710fcdf9dc2b4c6c599f74056061cedab4e6c03
9oodam <godamcity98@gmail.com>
2023-03-10 16:30:01 +0900

    day9(v02)

--- 20230310/sookjae.js
+++ 20230310/sookjae.js
@@ -1,21 +1,22 @@
 // 1번 문제
 
 let students = ["김씨", "박씨", "이씨", "최씨"];
-let award = ["이씨"];
+let award = ["박씨", "이씨"];
 
 // beststudent = students.filter(it => award.includes(it));
 // console.log(beststudent);
 
-let i = 0;
-let j = 0;
-for(award[i]; i < award.length; i++) {
-    for(students[j]; j < students.length; j++) {
-        if(students[j] == award[i]) {
-            console.log(students[j]);
-        }
-    }
-}
+let beststudent = [];
 
+for(let i = 0; i < award.length; i++) {
+    console.log("몇 번?")
+    for(let j = 0; j < students.length; j++) {
+        if(students[j] == award[i]) {
+            beststudent.push(students[j]);
+        }
+    }   
+}
+console.log(beststudent);
 
 
 // 2번 문제
