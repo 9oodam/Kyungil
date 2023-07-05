e5b6257457cb9a0f0fbf17d504abae03c776dd25
9oodam <godamcity98@gmail.com>
2023-07-05 14:36:04 +0900

    day 47

---
+++ 20230705/project1/src/components/Block.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Block = ({data, name, result}) => {
+  let temp = "";
+  if(name === "유저"){
+    temp = result;
+  }else{
+    // result == "무승부" 면 문제가 없어 얘는 놔둬도 상관 없고
+    // result == "이겼다" 면 반대로 졌다를 보여줘야하고
+    // result === "졌다" 면 반대로 이겼다를 보여줘야한다.
+    temp = result === "무승부" ? "무승부" : result === "이겼음" ? "졌다" : "이겼음"
+  }
+  return (
+    <div className='block'>
+        <div>{name}</div>
+        {/* react에서 이미지 import해오는 방법 */}
+        {/* 리액트에서 가장 많이 사용하는 조건부 렌더링 값이 있으면 값을 사용해라라는 구문 */}
+        {/* 값이 없으면 페이지가 터지기 때문에 */}
+        <img src={data && data.img} alt="" />
+        <div>{temp}</div>
+    </div>
+  )
+}
+
+export default Block
---
+++ 20230705/project1/src/img/index.js
@@ -0,0 +1,6 @@
+import img01 from "./scissors.png";
+import img02 from "./rock.png";
+import img03 from "./paper.png";
+
+// 다수 내보낼때
+export {img01, img02, img03};
